06c55951ca143ba6b4f91e7b3b65f2140390c2ed
arlox917 <olmosalejandra867@gmail.com>
2025-12-07 15:49:44 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'gateway01-privatelink.us-west-2.prod.aws.tidbcloud.com',
+  host: 'gateway01.us-west-2.prod.aws.tidbcloud.com',
   port: 4000,
-  user: '2UJfnfeYBh113YFR.root',
+  user: '2UJfnfeYBh113YF.root',
   password: 'FIx4jftwCWtZ2cuk',
   database: 'flightdb',
   ssl: {

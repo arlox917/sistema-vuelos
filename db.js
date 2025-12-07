31c8db70fa867a0986a160947ce9e5b2c5c559f9
arlox917 <olmosalejandra867@gmail.com>
2025-12-07 14:27:25 -0600

    Update database connection to use promise and AWS

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'admin',
-  password: 'admin',
-  database: 'flightdb'
+  host: 'gateway01-privatelink.us-west-2.prod.aws.tidbcloud.com',
+  port: 4000,
+  user: '2UJfnfeYBh113YFR.root',
+  password: 'FIx4jftwCWtZ2cuk',
+  database: 'flightdb',
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = db;
+

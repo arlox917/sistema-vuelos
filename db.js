d961d86a6b40ccb70870cc8440607d5a1e905619
arlox917 <olmosalejandra867@gmail.com>
2025-12-08 05:36:53 -0600

    Change database password in db.js
    Updated database password for security reasons.

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
   host: 'gateway01.us-west-2.prod.aws.tidbcloud.com',
   port: 4000,
   user: '2UJfnfeYBh113YF.root',
-  password: 'FIx4jftwCWtZ2cuk',
+  password: 'ndgUfibBf5gqo691',
   database: 'flightdb',
   ssl: {
     minVersion: 'TLSv1.2',
